04432087366836f05b62477b04b823a2273294e2
Rohit Karadkar <rohit.karadkar@thoughtworks.com>
2023-09-06 14:06:25 +0530

    Fix sample test

--- apps/counter/test/sample_test.dart
+++ apps/counter/test/sample_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:homescreen_feature/homescreen_feature.dart';
-
-void main() {
+void main(){
   test('sample test', () {
     expect(2 + 2, 4);
   });
